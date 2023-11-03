82a05b3f13bc5a4755233935e9b0392b1763651b
ComandanteVICNO <franciscojsmgoncalves@gmail.com>
2023-11-03 14:04:00 +0000

    teste fps 2

---
+++ Assets/LimitFramerate.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LimitFramerate : MonoBehaviour
+{
+    private int refreshRate = 60;
+
+    public float updateInterval = 0.5f; // How often to update the FPS value.
+    private float lastInterval;
+    private int frames = 0;
+    private float fps;
+    public TMP_Text fpsText;
+    
+    private void Start()
+    {
+        Application.targetFrameRate = refreshRate;
+
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
+    }
+
+    private void Update()
+    {
+        ++frames;
+        float timeNow = Time.realtimeSinceStartup;
+
+        if (timeNow > lastInterval + updateInterval)
+        {
+            fps = frames / (timeNow - lastInterval);
+            frames = 0;
+            lastInterval = timeNow;
+        }
+
+        fpsText.text = "FPS: " + fps;
+    }
+}
