7eed70a931a4aadf571bef4b2e646557887c5a3f
ComandanteVICNO <franciscojsmgoncalves@gmail.com>
2023-11-25 19:45:28 +0000

    more objects

--- Assets/Scritps/PlasticBinManager.cs
+++ Assets/Scritps/PlasticBinManager.cs
@@ -6,7 +6,6 @@ public class PlasticBinManager : MonoBehaviour
 {
     public ScoreManager scoreManager;
     public DragAndDrop dragNDrop;
-    StopClock stopClock;
     public GameObject pauseCanvas;
 
     private void Start()
@@ -20,12 +19,6 @@ public class PlasticBinManager : MonoBehaviour
         if (other.GetComponent<DragAndDrop>() == null) return;
         dragNDrop = other.GetComponent<DragAndDrop>();
 
-        if (other.GetComponent<StopClock>() != null) 
-        {
-            stopClock = other.GetComponent<StopClock>();
-            stopClock.canStop = true;
-        }
-
 
         if (dragNDrop.isDragging) return;
         else
@@ -33,10 +26,6 @@ public class PlasticBinManager : MonoBehaviour
             if (other.CompareTag("Plastic") || other.CompareTag("Metal"))
             {
                 scoreManager.IncreaseScore();
-
-                
-
-
                 Object.Destroy(other.gameObject);
             }
             else
@@ -50,13 +39,9 @@ public class PlasticBinManager : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         dragNDrop = null;
-        stopClock.canStop = false;
-        stopClock = null;
+
         
     }
 
-    private void Pause()
-    {
-        pauseCanvas.SetActive(true);
-    }
+    
 }
